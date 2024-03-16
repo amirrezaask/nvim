acc884530c1ea18ab0ea9268c5043396c976d52a
amirrezaask <raskarpour@gmail.com>
2024-03-16 04:07:20 +0330

    feat: mini.statusline

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -12,4 +12,32 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {})
+require("lazy").setup({
+	-- [gc] to comment region/line.
+	{ "numToStr/Comment.nvim", opts = {} },
+
+	-- Detect tabstop and shiftwidth automatically.
+	{ "tpope/vim-sleuth" },
+
+	-- Highlight TODO in comments
+	{ "folke/todo-comments.nvim", opts = {} },
+
+	-- Improved quick fix list experience
+	"kevinhwang91/nvim-bqf",
+
+	-- Pretty icons
+	{ "nvim-tree/nvim-web-devicons" },
+
+	require("plugins.colors"),
+	require("plugins.git"),
+	require("plugins.autocomplete"),
+	require("plugins.format"),
+	require("plugins.lsp"),
+	require("plugins.telescope"),
+	require("plugins.statusline"),
+	require("plugins.treesitter"),
+
+	-- Custom functionalities
+	require("plugins.rooter"),
+	require("plugins.notebook"),
+}, {})
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,29 +1,31 @@
--- statusline
-local mode_texts = {
-	n = "Normal",
-	i = "Insert",
-	v = "Visual",
-	c = "Complete",
+-- -- statusline
+-- local mode_texts = {
+-- 	n = "Normal",
+-- 	i = "Insert",
+-- 	v = "Visual",
+-- 	c = "Complete",
+-- }
+--
+-- function AmirrezaStatusLine()
+-- 	local statusline = ""
+-- 	local mode = vim.api.nvim_get_mode().mode
+-- 	if mode_texts[mode] ~= nil then
+-- 		mode = mode_texts[mode]
+-- 	end
+--
+-- 	statusline = statusline .. mode
+--
+-- 	local branch = ""
+-- 	if vim.b.gitsigns_head ~= nil then
+-- 		branch = "Git:" .. vim.b.gitsigns_head
+-- 		statusline = statusline .. " | " .. branch .. " |"
+-- 	end
+--
+-- 	return statusline .. " %r%h%w%q%m%f | %y"
+-- end
+--
+-- vim.opt.statusline = "%!v:lua.AmirrezaStatusLine()"
+
+return {
+	{ "echasnovski/mini.statusline", version = false, opts = {} },
 }
-
-function AmirrezaStatusLine()
-	local statusline = ""
-	local mode = vim.api.nvim_get_mode().mode
-	if mode_texts[mode] ~= nil then
-		mode = mode_texts[mode]
-	end
-
-	statusline = statusline .. mode
-
-	local branch = ""
-	if vim.b.gitsigns_head ~= nil then
-		branch = "Git:" .. vim.b.gitsigns_head
-		statusline = statusline .. " | " .. branch .. " |"
-	end
-
-	return statusline .. " %r%h%w%q%m%f | %y"
-end
-
-vim.opt.statusline = "%!v:lua.AmirrezaStatusLine()"
-
-return {}
