a983b4db23db4e2a2083cab205807bb302aded43
amirrezaask <raskarpour@gmail.com>
2025-03-20 06:39:26 +0330

    Work In Progress

--- init.lua
+++ init.lua
@@ -1,5 +1,23 @@
-require("sets")
-require("maps")
-require("load_plugins")
+vim.g.mapleader = " "
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd.colorscheme(os.getenv("NVIM_COLORSCHEME") or "rose-pine")
+require("lazy").setup({
+	spec = "custom",
+	change_detection = { notify = false },
+})
+
+vim.cmd.colorscheme(os.getenv("NVIM_COLORSCHEME") or "catppuccin-mocha")
