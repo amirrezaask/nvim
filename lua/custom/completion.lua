976b06b8c5ca2426642a5947df67581d55051124
amirrezaask <raskarpour@gmail.com>
2025-03-21 16:59:24 +0330

    Work In Progress

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -1,43 +1,43 @@
 return {
-	{ -- Autocomplete menu
-		"hrsh7th/nvim-cmp",
-		enabled = false,
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-		},
-		config = function()
-			local cmp_select = { behavior = require("cmp").SelectBehavior.Select }
-			local cmp = require("cmp")
-			cmp.setup({
-				preselect = require("cmp.types").cmp.PreselectMode.None,
-				snippet = {
-					expand = function(args)
-						vim.snippet.expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "buffer" },
-					{
-						name = "lazydev",
-						group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-					},
-				},
-			})
-		end,
-	},
+	-- { -- Autocomplete menu
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	enabled = false,
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-buffer",
+	-- 	},
+	-- 	config = function()
+	-- 		local cmp_select = { behavior = require("cmp").SelectBehavior.Select }
+	-- 		local cmp = require("cmp")
+	-- 		cmp.setup({
+	-- 			preselect = require("cmp.types").cmp.PreselectMode.None,
+	-- 			snippet = {
+	-- 				expand = function(args)
+	-- 					vim.snippet.expand(args.body)
+	-- 				end,
+	-- 			},
+	-- 			mapping = cmp.mapping.preset.insert({
+	-- 				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	-- 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	-- 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	-- 				["<CR>"] = cmp.mapping.confirm({ select = false }),
+	-- 				["<C-Space>"] = cmp.mapping.complete(),
+	-- 			}),
+	-- 			sources = {
+	-- 				{ name = "nvim_lsp" },
+	-- 				{ name = "buffer" },
+	-- 				{
+	-- 					name = "lazydev",
+	-- 					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"saghen/blink.cmp",
 		dependencies = { "rafamadriz/friendly-snippets" },
-		version = "v0.14.0",
+		version = "*",
 		opts = {
 			keymap = { preset = "enter" },
 			appearance = {
