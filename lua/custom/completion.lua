a322b8f0854266154121d26ef5d26ff1405740ad
amirrezaask <raskarpour@gmail.com>
2025-03-21 16:58:14 +0330

    Work In Progress

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -1,45 +1,58 @@
-return { -- Autocomplete menu
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		{
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
+return {
+	{ -- Autocomplete menu
+		"hrsh7th/nvim-cmp",
+		enabled = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
 		},
+		config = function()
+			local cmp_select = { behavior = require("cmp").SelectBehavior.Select }
+			local cmp = require("cmp")
+			cmp.setup({
+				preselect = require("cmp.types").cmp.PreselectMode.None,
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{
+						name = "lazydev",
+						group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+					},
+				},
+			})
+		end,
 	},
-	config = function()
-		local cmp_select = { behavior = require("cmp").SelectBehavior.Select }
-		local cmp = require("cmp")
-		cmp.setup({
-			preselect = require("cmp.types").cmp.PreselectMode.None,
-			snippet = {
-				expand = function(args)
-					vim.snippet.expand(args.body)
-				end,
+	{
+		"saghen/blink.cmp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		version = "v0.14.0",
+		opts = {
+			keymap = { preset = "enter" },
+			appearance = {
+				nerd_font_variant = "mono",
 			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{
-					name = "lazydev",
-					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			completion = {
+				documentation = {
+					auto_show = true,
 				},
 			},
-		})
-	end,
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
 }
